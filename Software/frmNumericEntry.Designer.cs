6c5a489104a4c56d2aca80c84d0b7bc9e9125bef
Corbin Voigt <voigtc@dwfritz.com>
2015-11-11 13:36:09 -0800

    Allow negative dollar amount on numeric input form

--- Software/frmNumericEntry.Designer.cs
+++ Software/frmNumericEntry.Designer.cs
@@ -55,6 +55,11 @@
             0,
             0,
             0});
+         this.numericUpDown1.Minimum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            -2147483648});
          this.numericUpDown1.Name = "numericUpDown1";
          this.numericUpDown1.Size = new System.Drawing.Size(149, 26);
          this.numericUpDown1.TabIndex = 1;
